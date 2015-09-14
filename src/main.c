715f1136506df9a8c33e2f13ba717c9ab2e18832
Corey Besmer <cbesmer@utexas.edu>
2015-09-14 12:33:51 -0500

    Fixed fish disappearing/font not rendering correct
    Resolved a memory leak that caused the watch face to allocate an infinite number of fishes, causing the fishes to die. Fishes now are only birthed once, and live forever.
    Resolved a similar issue with the font. Font was being loaded every second, and this caused issues with the font not loading correctly.

--- src/main.c
+++ src/main.c
@@ -20,14 +20,16 @@ static BitmapLayer *s_box_layer;
 static BitmapLayer *s_box_layer2;
 static BitmapLayer *s_box_layer3;
 static BitmapLayer *s_background_layer;
+static GBitmap *right_fish;
+static GBitmap *left_fish;
 static PropertyAnimation *s_box_animation;
-static TextLayer *s_time_layer;
+static Layer *s_time_layer;
 static GFont s_time_font;
 static GBitmap *s_background_bitmap;
 static GBitmap *s_fish_bitmap;
 static GBitmap *s_fish_bitmap2;
 static GBitmap *s_fish_bitmap3;
-
+char curTime[64];
 
 /*
  *  TIME STUFF
@@ -39,19 +41,19 @@ static void update_time() {
   struct tm *tick_time = localtime(&temp);
 
   // Create a long-lived buffer
-  static char buffer[] = "00:00";
+  strcpy(curTime, "00:00");
 
   // Write the current hours and minutes into the buffer
   if(clock_is_24h_style() == true) {
     // Use 24 hour format
-    strftime(buffer, sizeof("00:00"), "%H:%M", tick_time);
+    strftime(curTime, sizeof("00:00"), "%H:%M", tick_time);
   } else {
     // Use 12 hour format
-    strftime(buffer, sizeof("00:00"), "%I:%M", tick_time);
+    strftime(curTime, sizeof("00:00"), "%I:%M", tick_time);
   }
 
   // Display this time on the TextLayer
-  text_layer_set_text(s_time_layer, buffer);
+  layer_mark_dirty(s_time_layer);
 }
 
 static void tick_handler(struct tm *tick_time, TimeUnits units_changed) {
@@ -72,6 +74,7 @@ static void anim_stopped_handler(Animation *animation, bool finished, void *cont
 #endif
 
   // Schedule the next one, unless the app is exiting
+  //APP_LOG(APP_LOG_LEVEL_DEBUG, "animation stopped, finished: %d", finished); // why do some fish stop moving?
   if (finished) {
     next_animation(context); // figure out how to get who just finished
   }
@@ -85,11 +88,11 @@ static void anim_started_handler(Animation *animation, void *context) {
   property_animation_get_to_gpoint((PropertyAnimation *) animation, &to);
   if (from.x - to.x < 0) {
     // fish is going right
-    bitmap_layer_set_bitmap(layer, gbitmap_create_with_resource(RESOURCE_ID_ORANGE_FISH_RIGHT));
+    bitmap_layer_set_bitmap(layer, right_fish);
   }
   else {
     // fish is going left
-    bitmap_layer_set_bitmap(layer, gbitmap_create_with_resource(RESOURCE_ID_ORANGE_FISH_LEFT));
+    bitmap_layer_set_bitmap(layer, left_fish);
   }
 }
 
@@ -126,6 +129,19 @@ static void update_proc(Layer *layer, GContext *ctx) {
 }
 */
 
+void draw_time_text(Layer *this_layer, GContext *ctx){
+    graphics_context_set_text_color(ctx, GColorBlack);
+    
+    graphics_draw_text(ctx, curTime, s_time_font, GRect(1,21, 144,32), GTextOverflowModeFill, GTextAlignmentCenter, NULL);
+    graphics_draw_text(ctx, curTime, s_time_font, GRect(3,21, 144,32), GTextOverflowModeFill, GTextAlignmentCenter, NULL);
+    graphics_draw_text(ctx, curTime, s_time_font, GRect(1,23, 144,32), GTextOverflowModeFill, GTextAlignmentCenter, NULL);
+    graphics_draw_text(ctx, curTime, s_time_font, GRect(3,23, 144,32), GTextOverflowModeFill, GTextAlignmentCenter, NULL);
+    graphics_context_set_text_color(ctx, GColorWhite);
+ 
+    graphics_draw_text(ctx, curTime, s_time_font, GRect(2,22, 144,32), GTextOverflowModeFill, GTextAlignmentCenter, NULL);
+
+}
+
 static void main_window_load(Window *window) {
   Layer *window_layer = window_get_root_layer(window);
 
@@ -138,9 +154,9 @@ static void main_window_load(Window *window) {
   //layer_set_update_proc(bitmap_layer_get_layer(s_box_layer3), update_proc);
   
   // Create fish bitmaps
-  s_fish_bitmap = gbitmap_create_with_resource(RESOURCE_ID_ORANGE_FISH_RIGHT);
-  s_fish_bitmap2 = gbitmap_create_with_resource(RESOURCE_ID_ORANGE_FISH_RIGHT);
-  s_fish_bitmap3 = gbitmap_create_with_resource(RESOURCE_ID_ORANGE_FISH_RIGHT);
+  s_fish_bitmap = right_fish;
+  s_fish_bitmap2 = right_fish;
+  s_fish_bitmap3 = right_fish;
   bitmap_layer_set_compositing_mode(s_box_layer, GCompOpSet);
   bitmap_layer_set_compositing_mode(s_box_layer2, GCompOpSet);
   bitmap_layer_set_compositing_mode(s_box_layer3, GCompOpSet);
@@ -148,16 +164,12 @@ static void main_window_load(Window *window) {
   bitmap_layer_set_bitmap(s_box_layer2, s_fish_bitmap2);
   bitmap_layer_set_bitmap(s_box_layer3, s_fish_bitmap3);
   
-  // Create time TextLayer
-  s_time_layer = text_layer_create(GRect(0, 30, 144, 70));
-  text_layer_set_background_color(s_time_layer, GColorClear);
+  // set time font
+  s_time_font = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_KEY_HANGDJ_40));
   
-  #ifdef PBL_COLOR
-    text_layer_set_text_color(s_time_layer, GColorWhite);
-  #else
-    text_layer_set_text_color(s_time_layer, GColorBlack);
-  #endif
-  text_layer_set_text(s_time_layer, "00:00");
+  // Create time TextLayer
+  s_time_layer = layer_create(GRect(0, 0, 144, 168));
+  layer_set_update_proc(s_time_layer, draw_time_text);
   
   // Create GBitmap, then set to created BitmapLayer
   s_background_bitmap = gbitmap_create_with_resource(RESOURCE_ID_BACKGROUND_IMAGE_AQUARIUM);
@@ -167,13 +179,10 @@ static void main_window_load(Window *window) {
 
   // Improve the layout to be more like a watchface
   // Create GFont
-  s_time_font = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_KEY_HANGDJ_40));
 
-  text_layer_set_font(s_time_layer, s_time_font);
-  text_layer_set_text_alignment(s_time_layer, GTextAlignmentCenter);
 
   // Add it as a child layer to the Window's root layer
-  layer_add_child(window_layer, text_layer_get_layer(s_time_layer));
+  layer_add_child(window_layer, s_time_layer);
   layer_add_child(window_layer, bitmap_layer_get_layer(s_box_layer));
   layer_add_child(window_layer, bitmap_layer_get_layer(s_box_layer2));
   layer_add_child(window_layer, bitmap_layer_get_layer(s_box_layer3));
@@ -185,7 +194,7 @@ static void main_window_unload(Window *window) {
   bitmap_layer_destroy(s_box_layer2);
   bitmap_layer_destroy(s_box_layer3);
   // Destroy TextLayer
-  text_layer_destroy(s_time_layer);
+  layer_destroy(s_time_layer);
   
   // Unload GFont
   fonts_unload_custom_font(s_time_font);
@@ -206,6 +215,10 @@ static void init(void) {
   #else 
     window_set_background_color(s_main_window, GColorWhite);
   #endif
+    
+  // create fish
+  left_fish = gbitmap_create_with_resource(RESOURCE_ID_ORANGE_FISH_LEFT);
+  right_fish = gbitmap_create_with_resource(RESOURCE_ID_ORANGE_FISH_RIGHT);
     
   // Set handlers to manage the elements inside the Window
   window_set_window_handlers(s_main_window, (WindowHandlers) {
@@ -234,7 +247,13 @@ static void init(void) {
 static void deinit(void) {
   // Stop any animation in progress
   animation_unschedule_all();
-
+  gbitmap_destroy(right_fish);
+  gbitmap_destroy(left_fish);
+  gbitmap_destroy(s_background_bitmap);
+  gbitmap_destroy(s_fish_bitmap);
+  gbitmap_destroy(s_fish_bitmap2);
+  gbitmap_destroy(s_fish_bitmap3);
+  
   // Destroy main Window
   window_destroy(s_main_window);
 }
